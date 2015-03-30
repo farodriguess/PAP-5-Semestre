eb6795eb35a08d733705fe149190427877cefb2a
Francisco Anderson <contato@franciscoanderson.com>
2015-03-30 18:41:21 -0300

    Adicionado tela Editar Campeonato

---
+++ PAP-5-Semestre/src/com/example/pap/TelaEditarCampeonato.java
@@ -0,0 +1,21 @@
+package com.example.pap;
+
+import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class TelaEditarCampeonato extends Tela_Meus_Campeonatos {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.tela_editar_campeonato);
+	}
+	
+	
+
+
+}

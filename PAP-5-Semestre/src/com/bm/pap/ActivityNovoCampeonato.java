05b79b0fa737a84295fb82e7965160cbe54b9f9e
Francisco Anderson <contato@franciscoanderson.com>
2015-04-02 17:56:17 -0300

    Feito a tela Novo Campeonato

---
+++ PAP-5-Semestre/src/com/bm/pap/ActivityNovoCampeonato.java
@@ -0,0 +1,15 @@
+package com.bm.pap;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ActivityNovoCampeonato extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_activity_novo_campeonato);
+	}
+}

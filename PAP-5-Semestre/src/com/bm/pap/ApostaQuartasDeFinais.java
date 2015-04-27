20d698e8b6eaf4d42ab79c876d6bc7c3e9fd3327
Allan <Allan@Allan-PC>
2015-04-26 21:14:39 -0300

    add telas oitavas quartas semi terceiro lugar e final

---
+++ PAP-5-Semestre/src/com/bm/pap/ApostaQuartasDeFinais.java
@@ -0,0 +1,30 @@
+package com.bm.pap;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class ApostaQuartasDeFinais extends PrincipalActivity{
+	
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_aposta_quartas);
+		
+		
+		Spinner spinner1 = (Spinner) findViewById(R.id.spinner1);
+	    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+	    this, R.array.Gols, android.R.layout.simple_spinner_item);
+	    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	    spinner1.setAdapter(adapter);
+	    
+	    Spinner spinner2 = (Spinner) findViewById(R.id.spinner2);
+	    ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(
+	    this, R.array.Gols, android.R.layout.simple_spinner_item);
+	    adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	    spinner2.setAdapter(adapter2);
+	
+	
+
+}
+	
+}
---
+++ PAP-5-Semestre/src/com/bm/pap/ApostaSemifinal.java
@@ -0,0 +1,46 @@
+package com.bm.pap;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class ApostaSemifinal extends PrincipalActivity{
+	
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_aposta_semifinais);
+		
+		
+		
+		Spinner spinner1 = (Spinner) findViewById(R.id.spinner1);
+	    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+	    this, R.array.Gols, android.R.layout.simple_spinner_item);
+	    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	    spinner1.setAdapter(adapter);
+	    
+	    Spinner spinner2 = (Spinner) findViewById(R.id.spinner2);
+	    ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(
+	    this, R.array.Gols, android.R.layout.simple_spinner_item);
+	    adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	    spinner2.setAdapter(adapter2);
+	    
+	    Spinner spinner3 = (Spinner) findViewById(R.id.spinner3);
+	    ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(
+	    this, R.array.Gols, android.R.layout.simple_spinner_item);
+	    adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	    spinner3.setAdapter(adapter3);
+
+	    
+	    Spinner spinner4 = (Spinner) findViewById(R.id.spinner4);
+	    ArrayAdapter<CharSequence> adapter4 = ArrayAdapter.createFromResource(
+	    this, R.array.Gols, android.R.layout.simple_spinner_item);
+	    adapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	    spinner4.setAdapter(adapter4);
+	
+	
+	
+	
+
+}
+
+}

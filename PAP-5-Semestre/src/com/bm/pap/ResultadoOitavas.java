7796c9f48c4a2f9dbea2f4fe0d9d10c1fea5abe3
Allan <Allan@Allan-PC>
2015-05-02 20:04:59 -0300

    adicionado as telas ver resultados tertrapracary fazer essa porra

---
+++ PAP-5-Semestre/src/com/bm/pap/ResultadoOitavas.java
@@ -0,0 +1,12 @@
+package com.bm.pap;
+
+import android.os.Bundle;
+
+public class ResultadoOitavas extends PrincipalActivity{
+	
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_ver_resultado_oitavas);
+	}
+}

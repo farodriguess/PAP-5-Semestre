1918489c03faa348854f3e3a503fb2933d96b484
Allan <Allan@192.168.1.101>
2015-05-01 22:42:05 -0300

    adicionado as telas 01-05-2015

--- PAP-5-Semestre/src/com/bm/pap/RegrasActivity.java
+++ PAP-5-Semestre/src/com/bm/pap/RegrasActivity.java
@@ -4,12 +4,33 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class RegrasActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_regras);
+		setContentView(R.layout.activity_regras2);
+		
+		
+		Spinner spinner1 = (Spinner) findViewById(R.id.spinner1);
+	    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+	    this, R.array.Pontos, android.R.layout.simple_spinner_item);
+	    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	    spinner1.setAdapter(adapter);
+	    
+	    Spinner spinner2 = (Spinner) findViewById(R.id.spinner2);
+	    ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(
+	    this, R.array.Pontos, android.R.layout.simple_spinner_item);
+	    adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	    spinner2.setAdapter(adapter2);
+	    
+	    Spinner spinner3 = (Spinner) findViewById(R.id.spinner3);
+	    ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(
+	    this, R.array.Pontos, android.R.layout.simple_spinner_item);
+	    adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	    spinner3.setAdapter(adapter3);
 	}
 }

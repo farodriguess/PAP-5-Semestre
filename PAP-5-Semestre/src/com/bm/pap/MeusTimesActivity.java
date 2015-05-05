ca82e1d0e056710a81bc40a3a7d234d14522d695
Allan <Allan@Allan-PC.home>
2015-05-05 00:48:18 -0300

    arrumado o scrooll add time

--- PAP-5-Semestre/src/com/bm/pap/MeusTimesActivity.java
+++ PAP-5-Semestre/src/com/bm/pap/MeusTimesActivity.java
@@ -1,15 +1,45 @@
 package com.bm.pap;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MeusTimesActivity extends Activity {
+
+	private List<String> equipes;
+	private ArrayAdapter<String> adapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_meus_times);
+
+		equipes = new ArrayList<String>();
+		ListView lista = (ListView) findViewById(R.id.list);
+		adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, equipes);
+		lista.setAdapter(adapter);
 	}
+	
+	public void adicionarEquipe(View view){
+		EditText nomeTime = (EditText)
+				findViewById(R.id.nomeTime);
+				if(nomeTime.getText().toString().trim().length() > 0) {
+				equipes.add(nomeTime.getText().toString());
+				adapter.notifyDataSetChanged();
+				nomeTime.setText("");
+				} else {
+				Toast.makeText(this, "Campo nome do time em branco!",
+				Toast.LENGTH_SHORT).show();
+		
+				}
+	}
+	
 }

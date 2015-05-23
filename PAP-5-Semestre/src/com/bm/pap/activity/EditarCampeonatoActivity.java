6f6ada6f57d833f361b8218eee253f29d5295955
Jeferson <kjeferson@hotmail.com>
2015-05-23 16:30:26 -0300

    Implementado pop ups na tela meus campeonatos e adicionado algumas activity

--- PAP-5-Semestre/src/com/bm/pap/activity/EditarCampeonatoActivity.java
+++ PAP-5-Semestre/src/com/bm/pap/activity/EditarCampeonatoActivity.java
@@ -1,27 +1,204 @@
 package com.bm.pap.activity;
 
-import com.bm.pap.R;
+import java.text.DateFormat;
+import java.util.Calendar;
 
+import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class EditarCampeonatoActivity extends MeusCampeonatosActivity {
+import com.bm.pap.R;
+import com.bm.pap.entity.Campeonato;
+import com.bm.pap.entity.Usuario;
 
-	@Override
+public class EditarCampeonatoActivity extends Activity {
+	private Campeonato campeonato;
+	private EditText nome;
+	private RadioGroup rg;
+	private DateFormat formete2;
+	private Calendar calendarDataInicio;
+	private Calendar calendarDatafim;
+	private TextView campoDataInicio;
+	private TextView campoDataFim;
+
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_editar_campeonato);
+		setContentView(R.layout.activity_editar_info_campeonato);
+//		campoDataInicio = (TextView) findViewById(R.id.datainicio);
+//		campoDataFim = (TextView) findViewById(R.id.datafim);
+//		calendarDataInicio = Calendar.getInstance();
+//		calendarDatafim = Calendar.getInstance();
+//		formete2 = DateFormat.getDateInstance();
+//		nome = (EditText) findViewById(R.id.campoNomeCampeonato);
+//		// iniciar campeonato e setar regras
+//		Intent intencao = getIntent();
+//		Bundle info = intencao.getExtras();
+//		if (info.getSerializable("campeonato") != null) {
+//			campeonato = (Campeonato) info.getSerializable("campeonato");
+//		
+//		} else {
+//			
+//			campeonato = (Campeonato) info.getSerializable("editarCampeonato");
+//		}
+//		
+//	
+//		
+//		nome.setText(campeonato.getNome());
+//		RadioButton radioOito = (RadioButton) findViewById(R.id.radioButton1);
+//		RadioButton radioDezesseis = (RadioButton) findViewById(R.id.radioButton2);
+//		calendarDataInicio.setTime(campeonato.getDataInicio());
+//		calendarDatafim.setTime(campeonato.getDataFinal());
+//		campoDataInicio.setText(formete2.format(calendarDataInicio
+//				.getTime()));
+//		campoDataFim.setText(formete2.format(calendarDatafim.getTime()));
+//		if (campeonato.getQtdEquipe() == 8) {
+//			radioOito.setChecked(true);
+//			radioDezesseis.setChecked(false);
+//		} else {
+//			if (campeonato.getQtdEquipe() == 16) {
+//				radioDezesseis.setChecked(true);
+//				radioOito.setChecked(false);
+//			}
+//		}
+//		
+//		
+//		
+		
 	}
-	
-	public void botaoAjustarRegras(View view){
-		Intent intent = new Intent(this,RegrasActivity.class);
-		startActivity(intent);
-	}
-	
-	
 
+	public void setDataInicio(View view) {
+		new DatePickerDialog(EditarCampeonatoActivity.this, dataInicioListener,
+				calendarDataInicio.get(Calendar.YEAR),
+				calendarDataInicio.get(Calendar.MONTH),
+				calendarDataInicio.get(Calendar.DAY_OF_MONTH)).show();
+	}
+
+	private DatePickerDialog.OnDateSetListener dataInicioListener = new DatePickerDialog.OnDateSetListener() {
+
+		@Override
+		public void onDateSet(DatePicker view, int year, int monthOfYear,
+				int dayOfMonth) {
+			calendarDataInicio.set(Calendar.YEAR, year);
+			calendarDataInicio.set(Calendar.MONTH, monthOfYear);
+			calendarDataInicio.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+			campoDataInicio.setText(formete2.format(calendarDataInicio
+					.getTime()));
+
+		}
+	};
+
+	public void setDataFim(View view) {
+		new DatePickerDialog(EditarCampeonatoActivity.this, dataFimListener,
+				calendarDatafim.get(Calendar.YEAR),
+				calendarDataInicio.get(Calendar.MONTH),
+				calendarDatafim.get(Calendar.DAY_OF_MONTH)).show();
+	}
+
+	private DatePickerDialog.OnDateSetListener dataFimListener = new DatePickerDialog.OnDateSetListener() {
+
+		@Override
+		public void onDateSet(DatePicker view, int year, int monthOfYear,
+				int dayOfMonth) {
+
+			calendarDatafim.set(Calendar.YEAR, year);
+			calendarDatafim.set(Calendar.MONTH, monthOfYear);
+			calendarDatafim.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+			campoDataFim.setText(formete2.format(calendarDatafim.getTime()));
+
+		}
+	};
+
+	public void btnAjustarRegras(View view) {
+		rg = (RadioGroup) findViewById(R.id.qtdEquipes);
+		int op = rg.getCheckedRadioButtonId();
+		if (op == R.id.radioButton1) {
+			campeonato.setQtdEquipe(8);
+		} else {
+			if (op == R.id.radioButton2) {
+				campeonato.setQtdEquipe(16);
+			}
+		}
+		;
+		campeonato.setDataInicio(calendarDataInicio.getTime());
+		campeonato.setDataFinal(calendarDatafim.getTime());
+		campeonato.setNome(nome.getText().toString());
+		Intent intent_ajustar_regras = new Intent(this, RegrasActivity.class);
+		intent_ajustar_regras.putExtra("campeonato", campeonato);
+		startActivityForResult(intent_ajustar_regras, 1);
+	}
+
+	// testar data allan 14-05
+
+	// Alterado as lógicas do botão Criar Partidas (Jeferson)
+	public void btnCriarPartidas(View view) {
+
+		if (nome.getText().toString().trim().length() > 0) {
+			if (nome.getText().toString().length() <= 30) {
+				campeonato.setNome(nome.getText().toString());
+				campeonato.setDataInicio(calendarDataInicio.getTime());
+				campeonato.setDataFinal(calendarDatafim.getTime());
+				rg = (RadioGroup) findViewById(R.id.qtdEquipes);
+				int op = rg.getCheckedRadioButtonId();
+
+				if (op == R.id.radioButton1) {
+					campeonato.setQtdEquipe(8);
+				} else {
+					if (op == R.id.radioButton2) {
+						campeonato.setQtdEquipe(16);
+					}
+				}
+				;
+
+				if (calendarDataInicio.equals(Calendar.getInstance())
+						|| calendarDataInicio.after(Calendar.getInstance())) {
+					if (calendarDatafim.equals(calendarDataInicio)
+							|| calendarDatafim.after(calendarDataInicio)) {
+						Intent intent = new Intent(this,
+								SelecionarTimeCriarPartidas.class);
+						intent.putExtra("campeonato", campeonato);
+						startActivity(intent);
+					} else {
+						Toast.makeText(this,
+								"Data final inferior a data inicial!",
+								Toast.LENGTH_LONG).show();
+						calendarDatafim = calendarDataInicio;
+						campoDataFim.setText(formete2.format(calendarDatafim
+								.getTime()));
+					}
+
+				} else {
+					Toast.makeText(this, "Data inicial inferior a data atual!",
+							Toast.LENGTH_LONG).show();
+					calendarDataInicio = Calendar.getInstance();
+					campoDataInicio.setText(formete2.format(calendarDataInicio
+							.getTime()));
+				}
+
+			} else {
+				Toast.makeText(this,
+						"A equipe deve ter no máximo 30 caracteres!",
+						Toast.LENGTH_SHORT).show();
+			}
+
+		} else {
+			Toast.makeText(this, "Campo nome em branco!", Toast.LENGTH_SHORT)
+					.show();
+		}
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// TODO Auto-generated method stub
+		this.campeonato = (Campeonato) data.getSerializableExtra("campeonato");
+
+	}
 
 }
--- PAP-5-Semestre/src/com/bm/pap/activity/MeusCampeonatosActivity.java
+++ PAP-5-Semestre/src/com/bm/pap/activity/MeusCampeonatosActivity.java
@@ -5,14 +5,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
 
 import com.bm.pap.R;
 import com.bm.pap.entity.Campeonato;
+import com.bm.pap.entity.Equipe;
 import com.bm.pap.entity.Usuario;
 import com.bm.pap.service.generic.ClientPostWSTask;
 import com.bm.pap.service.generic.ObjectLoadedListener;
@@ -28,38 +35,49 @@ public class MeusCampeonatosActivity extends Activity implements
 	private List<Campeonato> campeonatos;
 	private ArrayAdapter<String> adapter;
 	private Usuario usuario;
+	private AlertDialog alerta;
+	private AlertDialog alerta2;
+	private ListView lista;
+	private List<String> nomeCampeonato;
+	private String campeonato;
 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_meus_campeonatos);
 
 		Intent intencao = getIntent();
-		Bundle bundle = intencao.getExtras();		
+		Bundle bundle = intencao.getExtras();
 		usuario = (Usuario) bundle.getSerializable("usuario");
-		
-		if(usuario!=null){
 
-		// fazer lógica para carregar lista de campeonato e colocar na list
-		// List<Campeonato>e= new ArrayList<Equipe>();
-		ClientPostWSTask task = new ClientPostWSTask(this, CAMINHOCAMPEONATOS,
-				this, new Gson().toJson(usuario), CAMPEONATOS);
-		task.execute();
+		if (usuario != null) {
+
+			// fazer lógica para carregar lista de campeonato e colocar na list
+			// List<Campeonato>e= new ArrayList<Equipe>();
+			ClientPostWSTask task = new ClientPostWSTask(this,
+					CAMINHOCAMPEONATOS, this, new Gson().toJson(usuario),
+					CAMPEONATOS);
+			task.execute();
 		}
+
+		lista = (ListView) findViewById(R.id.listaMeusCampeonatos);
+		lista.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> adapter, View view,
+					int arg2, long arg3) {
+				campeonato = ((TextView) view).getText().toString();
+				opcoes();
+			}
+		});
 
 	}
 
 	public void botaoNovo(View view) {
 		Intent intent = new Intent(this, NovoCampeonatoActivity.class);
-	
+
 		intent.putExtra("usuario", usuario);
-	
+
 		startActivity(intent);
 
-	}
-
-	public void botaoEditar(View view) {
-		Intent intent = new Intent(this, EditarCampeonatoActivity.class);
-		startActivity(intent);
 	}
 
 	@Override
@@ -69,37 +87,224 @@ public class MeusCampeonatosActivity extends Activity implements
 		switch (solicitante) {
 		case CAMPEONATOS: {
 			Type collectionType = new TypeToken<List<Campeonato>>() {
-			}.getType();			
+			}.getType();
 			campeonatos = new Gson().fromJson(obj, collectionType);
 			carregarGrid();
 		}
-
 			break;
-
 		default:
 			break;
 		}
-
-		
-
-	
-
 	}
-	
-	private void carregarGrid(){
-		// a list campeonatos terá os nomes e status da lista de meusCampeonatos
 
-		/*
-		 * for(campeonatos a : e){ campeonatos.add(a.getNome()+" "+
-		 * a.getStatus()); }
-		 */
-		List<String> nomeCampeonato = new ArrayList();
-		for(Campeonato c:campeonatos){
-			nomeCampeonato.add(c.getNome());
+	private void carregarGrid() {
+
+		nomeCampeonato = new ArrayList();
+		for (Campeonato c : campeonatos) {
+			nomeCampeonato.add(c.getNome() + "\nStatus: " + c.getStatus());
 		}
-		ListView lista = (ListView) findViewById(R.id.listaMeusCampeonatos);
-		adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, nomeCampeonato);
+
+		adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, nomeCampeonato);
 		lista.setAdapter(adapter);
 	}
 
+	private void opcoes() {
+		// Lista de itens
+		ArrayList<String> itens = new ArrayList<String>();
+		itens.add("Opções");
+		itens.add("Excluir");
+		ArrayAdapter arrayAdapter = new ArrayAdapter(this,
+				R.layout.item_alerta, itens);
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+		builder.setTitle("Selecione uma opção:");
+
+		builder.setSingleChoiceItems(arrayAdapter, 0,
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface arg0, int arg1) {
+						if (arg1 == 0) {
+							opcoesCampeonato();
+						}
+						if (arg1 == 1) {
+							confirmarExclusao().show();
+						}
+						alerta.dismiss();
+					}
+				});
+		alerta = builder.create();
+		alerta.show();
+	}
+
+	public AlertDialog confirmarExclusao() {
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle("REMOVER CAMPEONATO");
+		builder.setMessage("Deseja remover o campeonato?");
+		builder.setPositiveButton("sim", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				// FAZER LÓGICA DE REMOVER CAMPEONATO DO BANCO
+				nomeCampeonato.remove(campeonato);
+				adapter.notifyDataSetChanged();
+			}
+		});
+
+		builder.setNegativeButton("não", null);
+		return builder.create();
+	}
+
+	private void opcoesCampeonato() {
+		// Lista de itens
+		ArrayList<String> itens = new ArrayList<String>();
+		itens.add("Editar info. do Campeonato");
+		itens.add("Editar Partidas");
+		itens.add("Ver Participantes");
+		itens.add("Ranking");
+		itens.add("Resultado Partida");
+		itens.add("Definir Status");
+		itens.add("Regras");
+
+		ArrayAdapter arrayAdapter = new ArrayAdapter(this,
+				R.layout.item_alerta, itens);
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+		builder.setTitle("Selecione uma opção:");
+
+		builder.setSingleChoiceItems(arrayAdapter, 0,
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface arg0, int arg1) {
+						if (arg1 == 0) {
+							editarInfoCampeonato();
+						}
+						if (arg1 == 1) {
+							editarPartida();
+						}
+						if (arg1 == 2) {
+							verParticipantes();
+						}
+						if (arg1 == 3) {
+							ranking();
+						}
+						if (arg1 == 4) {
+							resultadoPartida();
+						}
+						
+						if (arg1 == 5) {
+							definirStatus();
+						}
+						if (arg1 == 6) {
+							regras();
+						}
+						alerta2.dismiss();
+					}
+				});
+		alerta2 = builder.create();
+		alerta2.show();
+	}
+
+	public void editarInfoCampeonato() {
+		for (int i = 0; i < campeonatos.size(); i++) {
+			if (campeonatos
+					.get(i)
+					.getNome()
+					.equals(campeonato.substring(0,
+							campeonato.indexOf("Status") - 1))) {
+				Intent intent = new Intent(this, EditarCampeonatoActivity.class);
+				campeonatos.get(i).setUsuario(usuario);
+				intent.putExtra("editarCampeonato", campeonatos.get(i));
+				startActivity(intent);
+			}
+		}
+	}
+
+	public void editarPartida() {
+		for (int i = 0; i < campeonatos.size(); i++) {
+			if (campeonatos
+					.get(i)
+					.getNome()
+					.equals(campeonato.substring(0,
+							campeonato.indexOf("Status") - 1))) {
+				Intent intent = new Intent(this, EditarPartidaGAActivity.class);
+				campeonatos.get(i).setUsuario(usuario);
+				intent.putExtra("editarCampeonato", campeonatos.get(i));
+				startActivity(intent);
+			}
+		}
+	}
+
+	public void verParticipantes() {
+		for (int i = 0; i < campeonatos.size(); i++) {
+			if (campeonatos
+					.get(i)
+					.getNome()
+					.equals(campeonato.substring(0,
+							campeonato.indexOf("Status") - 1))) {
+				Intent intent = new Intent(this, VerParticipantesActivity.class);
+				campeonatos.get(i).setUsuario(usuario);
+				intent.putExtra("editarCampeonato", campeonatos.get(i));
+				startActivity(intent);
+			}
+		}
+	}
+
+	public void ranking() {
+		for (int i = 0; i < campeonatos.size(); i++) {
+			if (campeonatos
+					.get(i)
+					.getNome()
+					.equals(campeonato.substring(0,
+							campeonato.indexOf("Status") - 1))) {
+				Intent intent = new Intent(this, RankingActivity.class);
+				campeonatos.get(i).setUsuario(usuario);
+				intent.putExtra("editarCampeonato", campeonatos.get(i));
+				startActivity(intent);
+			}
+		}
+	}
+
+	public void resultadoPartida() {
+		for (int i = 0; i < campeonatos.size(); i++) {
+			if (campeonatos
+					.get(i)
+					.getNome()
+					.equals(campeonato.substring(0,
+							campeonato.indexOf("Status") - 1))) {
+				Intent intent = new Intent(this,
+						SelecionarResultadoActivity.class);
+				campeonatos.get(i).setUsuario(usuario);
+				intent.putExtra("editarCampeonato", campeonatos.get(i));
+				startActivity(intent);
+			}
+		}
+	}
+
+	public void definirStatus() {
+		for (int i = 0; i < campeonatos.size(); i++) {
+			if (campeonatos
+					.get(i)
+					.getNome()
+					.equals(campeonato.substring(0,
+							campeonato.indexOf("Status") - 1))) {
+				Intent intent = new Intent(this, StatusCampeonatoActivity.class);
+				campeonatos.get(i).setUsuario(usuario);
+				intent.putExtra("editarCampeonato", campeonatos.get(i));
+				startActivity(intent);
+			}
+		}
+	}
+
+	public void regras() {
+		for (int i = 0; i < campeonatos.size(); i++) {
+			if (campeonatos
+					.get(i)
+					.getNome()
+					.equals(campeonato.substring(0,
+							campeonato.indexOf("Status") - 1))) {
+				Intent intent = new Intent(this, VerRegrasActivity.class);
+				campeonatos.get(i).setUsuario(usuario);
+				intent.putExtra("editarCampeonato", campeonatos.get(i));
+				startActivity(intent);
+			}
+		}
+	}
 }
